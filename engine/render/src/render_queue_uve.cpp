c60fddc5385db4a04d142b53cda7ac161c5fc677
Kianmontalba <145521884+Kianmontalba@users.noreply.github.com>
2026-08-17 14:20:40 +0000

    render: add bounded particle render bridge

--- engine/render/src/render_queue_uve.cpp
+++ engine/render/src/render_queue_uve.cpp
@@ -1,6 +1,5 @@
 // Copyright (c) 2026 UniVex Studios. All Rights Reserved.
 
-
 #include "uve/render/render_queue_uve.h"
 
 #include <algorithm>
@@ -12,6 +11,24 @@ void RenderQueueUVE::SortUVE() {
               [](const RenderItemUVE& lhs, const RenderItemUVE& rhs) { return lhs.sortDepth < rhs.sortDepth; });
     std::sort(transparentItems.begin(), transparentItems.end(),
               [](const RenderItemUVE& lhs, const RenderItemUVE& rhs) { return lhs.sortDepth > rhs.sortDepth; });
+    std::sort(particleItems.begin(), particleItems.end(), [](const ParticleRenderItemUVE& lhs,
+                                                             const ParticleRenderItemUVE& rhs) {
+        if (lhs.sortDepth != rhs.sortDepth) {
+            return lhs.sortDepth > rhs.sortDepth;
+        }
+        if (lhs.entity.index != rhs.entity.index) {
+            return lhs.entity.index < rhs.entity.index;
+        }
+        if (lhs.entity.generation != rhs.entity.generation) {
+            return lhs.entity.generation < rhs.entity.generation;
+        }
+        return lhs.sequence < rhs.sequence;
+    });
+}
+
+void RenderQueueUVE::AppendParticleSnapshotUVE(const ParticleRenderSnapshotUVE& snapshot) {
+    particleItems.insert(particleItems.end(), snapshot.items.begin(), snapshot.items.end());
+    particleItemsTruncated = particleItemsTruncated || snapshot.truncated;
 }
 
 } // namespace UVE::Render
